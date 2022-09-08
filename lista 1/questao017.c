1da9900793b7a5180aa56cbc2eeac9de530e6bfc
jppn123 <59260587+jppn123@users.noreply.github.com>
2022-09-08 19:30:23 -0300

    Update questao017.c

--- lista 1/questao017.c
+++ lista 1/questao017.c
@@ -4,7 +4,7 @@ int main(){
     int x;
     printf("digite um numero: ");
     scanf("%d", &x);
-    printf("valor do modulo: %d", -x);
+    printf("valor do modulo: %d", ~x + 1);
 
     return 0;
 }
