f5c772e2c751b02fb1862dbfcf60da43a2c640c8
jppn123 <59260587+jppn123@users.noreply.github.com>
2022-09-30 17:56:59 -0300

    Update questao08.2.c

--- lista 3/questao08.2.c
+++ lista 3/questao08.2.c
@@ -8,21 +8,23 @@ void print(int x[], int ta);
 int main(){
 	int vet[tam];
 	srand(time(NULL));
+	
 	for(int i = 0; i < tam; i++){
-    	vet[i] = rand() % 101;
+    		vet[i] = rand() % 101;
 	}
+	
 	puts("vetor não ordenado: ");
 	print(vet, tam);
 	printf("\n");
     
 	for(int x = 0; x < tam-1; x++){
-    	for(int i = 0; i < tam-1; i++){
-        	if(vet[i] > vet[i+1]){
-            	vet[i+1] = vet[i+1] ^ vet[i];
-            	vet[i] = vet[i] ^ vet[i+1];
-            	vet[i+1] = vet[i+1] ^ vet[i];
-        	}
-    	}
+    		for(int i = 0; i < tam-1; i++){
+        		if(vet[i] > vet[i+1]){
+            		vet[i+1] = vet[i+1] ^ vet[i];
+            		vet[i] = vet[i] ^ vet[i+1];
+            		vet[i+1] = vet[i+1] ^ vet[i];
+        		}
+    		}
 	}
     
     
@@ -35,6 +37,6 @@ int main(){
 
 void print(int x[], int ta){
 	for(int i = 0; i<ta; i++){
-    	printf("%d ", x[i]);
+    		printf("%d ", x[i]);
 	}
 }
