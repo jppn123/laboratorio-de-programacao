008492c7166681cb609d6f629c0b9d39be128bfb
jppn123 <59260587+jppn123@users.noreply.github.com>
2022-10-05 08:55:05 -0300

    Create questao002.c

---
+++ lista 4/questao002.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int x, y, troca;
+    int *p1 = &x, *p2 = &y;
+
+    puts("digite o valor de x: ");
+    scanf("%d", &x);
+    puts("digite o valor de y: ");
+    scanf("%d", &y);
+
+    *p1 = *p1 ^ *p2;
+    *p2 = *p1 ^ *p2;
+    *p1 = *p1 ^ *p2;
+    
+    puts("após a troca: ");
+
+    printf("valor de x: %d\n", *p1);
+    printf("valor de y: %d\n", *p2);
+     
+
+    return 0;
+}
