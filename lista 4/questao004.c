0b3ae1623a77dc1fe06115ac30142cdeccaf1971
jppn123 <59260587+jppn123@users.noreply.github.com>
2022-10-17 09:26:36 -0300

    Update questao004.c

--- lista 4/questao004.c
+++ lista 4/questao004.c
@@ -4,19 +4,16 @@
 
 #define tam 10
 
-
-void imprimirVet(float *p, int t);
 int main(){
     float vet[tam];
     srand(time(NULL));
+    
     for(int i = 0; i < tam; i++){
         vet[i] = rand() % 10;
     }
-    imprimirVet(vet, tam);
+    for(int i = 0; i < tam; i++){
+        printf("%.2f [%p]\n", *(vet + i), vet + i);
+    }
     return 0;
 }
-void imprimirVet(float *p, int t){
-    for(int i = 0; i < t; i++){
-        printf("[%p] %.2f\n", p + i, *(p + i));
-    }
-}
+
