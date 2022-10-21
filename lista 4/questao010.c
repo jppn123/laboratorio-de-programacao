554c6b92cd1101c766f71f2ad0ae8d4ef11119f2
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2022-10-21 16:46:44 -0300

    Update questao010.c

--- lista 4/questao010.c
+++ lista 4/questao010.c
@@ -3,28 +3,7 @@
 #include <time.h>
 #define tam 5
 
-void print(int *, int);
 int main(){
-	int vet1[tam], vet2[tam];
-    srand(time(NULL));
-    for(int i = 0; i< tam; i++){
-        *(vet1 + i) = rand() % tam;
-    }
-    for(int i = 0; i< tam; i++){
-       
-        for(int x = 0; x < tam; x++){
-            if(vet2 == i){
-                vet2[x] = x; 
-            }
-        }
-    }
-    
-    print(vet2, tam);
 	return 0;
 }
 
-void print(int *x, int ta){
-	for(int i = 0; i<ta; i++){
-    		printf("%d ", *(x + i));
-	}
-}
