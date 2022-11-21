e7a2bfcf3ac100624a87386ba5b426fb1bd5442d
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2022-11-21 09:48:45 -0300

    Update questao001.c

--- lista 5/questao001.c
+++ lista 5/questao001.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[]){
 
     v1 = atoi(argv[1]);
     v2 = atoi(argv[2]);
-    op = atoi(argv[3]);
+    op = atoi(argv[3]) - 1;
 
     (*pf[op])(v1, v2, &val);
 
-    printf("resultado: %d", val);
+    printf("resultado: %d\n", val);
     return 0;
 }
 void soma(int a, int b, int *c){
