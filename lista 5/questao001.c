a54f57c25f051253a8fe41362b5811642a5b1d10
João Pedro Barros de Moura <59260587+jppn123@users.noreply.github.com>
2022-11-21 09:47:43 -0300

    Create questao001.c

---
+++ lista 5/questao001.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void soma(int, int, int *);
+void multi(int, int, int *);
+
+int main(int argc, char *argv[]){
+    void (*pf[])() = {soma, multi};
+    int val;
+    int v1 , v2, op;
+
+    v1 = atoi(argv[1]);
+    v2 = atoi(argv[2]);
+    op = atoi(argv[3]);
+
+    (*pf[op])(v1, v2, &val);
+
+    printf("resultado: %d", val);
+    return 0;
+}
+void soma(int a, int b, int *c){
+    *c = a + b;
+}
+void multi(int a, int b, int *c){
+    *c = a * b;
+}
